cd90443ad4e5ef67d139f748b1f65201e04c8a3c
JoHannah Joy <jojojoy2012@gmail.com>
2020-04-08 14:57:55 -0700

    complete grocery list Lab 1

--- code/johannah/grocery_list_lab_1/grocery_list_app/views.py
+++ code/johannah/grocery_list_lab_1/grocery_list_app/views.py
@@ -1,31 +1,25 @@
 from django.http import HttpResponseRedirect
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
 # from django.template import loader
 from django.utils import timezone
 from .models import GroceryItem
 
-# from .forms import *
-
 # Create your views here.
-
 def index(request):
   # template = loader.get_template('grocery_list/index.html')
   # form = GroceryItemForm()
   incompleted_items = GroceryItem.objects.filter(completed=False).order_by('created_date')
   completed_items = GroceryItem.objects.filter(completed=True).order_by('completed_date')
   context = {
-    'incompleted_items': 'Hello, world.'
-    'completed_items': GroceryItem.objects.all(),
-    # 'form': form
+    'incompleted_items': incompleted_items,
+    'completed_items': completed_items
   }
   # return HttpResponse(template.render(context, request))
-  return render(request, 'grocery_list_app/index.html', {'items': items})
+  return render(request, 'grocery_list/index.html', context)
 
 
-def add(request, item_id):
+def add(request):
   if request.POST['text_description']:
     GroceryItem.objects.create(created_date=timezone.now(), text_description=request.POST['text_description'])
   # template = loader.get_template('item.html')
