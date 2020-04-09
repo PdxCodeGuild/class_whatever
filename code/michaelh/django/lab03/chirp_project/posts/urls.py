4eb17235e0f58e255d824e072c456b15ac9d19d0
Michael Himmelsbach <himmelsm@gmail.com>
2020-04-09 14:16:52 -0700

    Lab 3 partially complete

---
+++ code/michaelh/django/lab03/chirp_project/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts' # for namespacing
+urlpatterns = [
+    path('chirp_index', views.chirp_index, name='chirp_index'),
+    path('post_new', views.post_new, name='post_new'),
+    # path('add', views.add, name='add'),
+]
