c09753a505ffbb5d586c4edf8a7cad87eb094916
TinaShreves <tina.l.shreves@gmail.com>
2020-04-09 15:38:06 -0700

    lab2

--- code/tina/django/url_project/shorty/models.py
+++ code/tina/django/url_project/shorty/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class URL(models.Model):
-    full_url = models.URLField(unique=True)
-    url_hash = models.URLField(unique=True)
+    full_url = models.URLField()
+    url_hash = models.CharField(max_length=6)
     clicks = models.IntegerField(default=0)
     create_at = models.DateTimeField(auto_now_add=True)
     
--- code/tina/django/url_project/shorty/urls.py
+++ code/tina/django/url_project/shorty/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'shorty'
 urlpatterns = [
 
-    path('')
+    path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('short_url/', views.short_url, name='short_url'),
+    path('redirect/', views.redirect, name='redirect')
     
 
 ]
