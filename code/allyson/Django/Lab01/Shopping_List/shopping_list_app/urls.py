8135cb4b8a6e20756bc515909b9c71f197c6a957
Allyson <Cl0udF1sh@hotmail.com>
2020-04-10 15:20:49 -0700

    Problem Child

--- code/allyson/Django/Lab01/Shopping_List/shopping_list_app/urls.py
+++ code/allyson/Django/Lab01/Shopping_List/shopping_list_app/urls.py
@@ -5,6 +5,6 @@ app_name = "shopping_list_app"
 
 urlpatterns = [
     path("", views.shopping_list_app, name="shopping_list_app"),
-    path('add_item', views.add_item, name='add'),
-    
+    path("add_item", views.add_item, name="add"),
+    path("remove_item", views.remove_item, name="remove"),
 ]
