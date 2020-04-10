fd07cde453961c25250cc81d8177b754bf2eefcd
TinaShreves <tina.l.shreves@gmail.com>
2020-04-10 16:53:25 -0700

    lab3

--- code/tina/django/chirp_project/post/views.py
+++ code/tina/django/chirp_project/post/views.py
@@ -5,4 +5,4 @@ from .models import Post
 
 def post_list(request):
     post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'post_list.html', {'post':post})
+    return render(request, 'post/post_list.html', {'post':post})
