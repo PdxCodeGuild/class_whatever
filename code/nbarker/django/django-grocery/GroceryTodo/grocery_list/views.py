a9b78a95958f9d0641c3bca1025cfea5619b96ce
Nick B <60245013+SickTightRad@users.noreply.github.com>
2020-04-11 19:34:01 -0700

    finished grocery lab

--- code/nbarker/django/django-grocery/GroceryTodo/grocery_list/views.py
+++ code/nbarker/django/django-grocery/GroceryTodo/grocery_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import GroceryItem
 from django.utils import timezone
@@ -7,8 +7,11 @@ from django.urls import reverse
 
 #'home' will list all of the items, created times, updated times, ***Need to find a way to sep into completed and not completed***
 def home(request):
-    all_grocery_items = GroceryItem.objects.all()
-    return render(request, 'grocery_list/dashboard.html', {'all_items': all_grocery_items},)
+    incomplete_items = GroceryItem.objects.filter(completed=False).order_by('timestamp')
+    completed_items = GroceryItem.objects.filter(completed=True).order_by('completed_date')
+    rabadaba = { 'incomplete_items': incomplete_items,
+                    'completed_items': completed_items }
+    return render(request, 'grocery_list/dashboard.html', rabadaba)
 
 #to add, we need to create an object, save it, and redirect to URL''
 def addGroceryItem(request):
@@ -16,12 +19,18 @@ def addGroceryItem(request):
     new_grocery_item.save()
     return HttpResponseRedirect('/home')
 
-def deleteGroceryItem(request, GroceryItem_id):
-    item_to_delete = GroceryItem.objects.get(id=GroceryItem_id)
+def deleteGroceryItem(request, pk):
+    item_to_delete = get_object_or_404(GroceryItem, pk=pk)
     item_to_delete.delete()
     return HttpResponseRedirect('/home')
 
-def completeGroceryItem(request, GroceryItem_id):
-    item_to_complete = GroceryItem.objects.get(id=GroceryItem_id)
-    item_to_complete.save()
+def completeGroceryItem(request, pk):
+    item = get_object_or_404(GroceryItem, pk=pk)
+    if item.completed:
+        item.completed = False
+        item.completed_date = None
+    else:
+        item.completed = True
+        item.completed_date = timezone.now()
+    item.save()
     return HttpResponseRedirect('/home')
