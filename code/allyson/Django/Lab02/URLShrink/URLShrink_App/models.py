f4c1b3add0701a28b00b06454a6c9c9170b168c3
Allyson <Cl0udF1sh@hotmail.com>
2020-04-13 13:53:01 -0700

    >> This weekend was full of everything but....<<

--- code/allyson/Django/Lab02/URLShrink/URLShrink_App/models.py
+++ code/allyson/Django/Lab02/URLShrink/URLShrink_App/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Shrink(models.Model):
-    URL = models.TextField()
-    code = models.CharField(max_length=200)
+    URL = models.CharField(max_length=600)
+    code = models.CharField(max_length=15)
+    chocobo = models.IntergerField(default=0)
 
     def __str__(self):
-        return "http://.../" + self.code
+        return self.URL
