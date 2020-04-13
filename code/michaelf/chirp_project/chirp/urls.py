7b3b61af20cd79604a64c6cea400c87a5ab5a26f
Michael Finn <mikeysight@gmail.com>
2020-04-13 11:13:19 -0700

    chirp revisions

--- code/michaelf/chirp_project/chirp/urls.py
+++ code/michaelf/chirp_project/chirp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('public/<str:author>/', views.public_profile, name='publicprofile'),
 ]
