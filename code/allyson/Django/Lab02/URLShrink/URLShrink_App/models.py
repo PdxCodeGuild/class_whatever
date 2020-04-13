80992880e64b774bd7dd0412d46c5ff4785aac81
Allyson <Cl0udF1sh@hotmail.com>
2020-04-13 14:37:06 -0700

    >> Still doesn't work fully...  But... You know it will. <<

--- code/allyson/Django/Lab02/URLShrink/URLShrink_App/models.py
+++ code/allyson/Django/Lab02/URLShrink/URLShrink_App/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Shrink(models.Model):
     URL = models.CharField(max_length=600)
     code = models.CharField(max_length=15)
-    chocobo = models.IntergerField(default=0)
+    # chocobo = models.IntergerField(default=0)
 
     def __str__(self):
         return self.URL
