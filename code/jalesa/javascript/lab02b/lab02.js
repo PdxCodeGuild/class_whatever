58bc94a23e0d83a83ede6576007b542a98c5bb21
jalesa <jalesa.wyatt@gmail.com>
2020-04-17 19:09:08 -0700

    lab02 almost finished

--- code/jalesa/javascript/lab02b/lab02.js
+++ code/jalesa/javascript/lab02b/lab02.js
@@ -22,20 +22,34 @@
 // elif operation == ("/"):
 //     print(first_number / second_number)
 
-operation = prompt("What is the operation you\'d like to perform? ")
-first_number = parseInt(prompt("What is the first number? "))
-second_number = parseInt(prompt("What is the second number?"))
+// ----------- JAVASCRIPT ANSWER --------------
+let submit_button = document.getElementById("submit");
+submit_button.addEventListener("click", run);
 
-if (operation == "*") {
-    prompt(first_number * second_number)
-} else if (operation == ("+")) {
-    prompt(first_number + second_number)
-} else if (operation == "-") {
-    prompt (first_number - second_number)
-} else if (operation == ("/")) {
-    prompt(first_number/second_number)
+
+let operator = document.getElementById("operation");
+let first_num = document.getElementById("first_num");
+let second_num = document.getElementById("second_num");
+
+function run() {
+    if (operation == "*") {
+        console.log((first_num.value) *(second_num.value))
+    } else if (operation == ("+")) {
+        (first_num.value + second_num.value)
+    } else if (operation == "-") {
+        (first_num.value - second_num.value)
+    } else if (operation == ("/")) {
+        (first_number.value/second_number.value)
+    }
+// --- appending text to input --
+    
+    input_element = document.createElement("input")
+    let text = document.createTextNode(`Your result is ${Number(first_num.value), operation.value, Number(second_num.value)}`);
+    // let text = document.createTextNode("hello world")
+    input_element.appendChild(text)
+    document.body.appendChild(input_element)
+
 }
-
 
 
 
@@ -78,28 +92,31 @@ if (operation == "*") {
 //         print(first_number / second_number)
 
 // --------------- Not running while loop  ------------
-while (true) {
-    user_input = prompt("Choose done if you would like to quit? ")
+// ----------- JAVASCRIPT ANSWER ----------------------
 
-    if (user_input == ("done")) {
-        prompt("Good-bye")
-        break
-    }
 
-    operation = prompt("What is the operation you would like to perform? ")
-    first_number = parseInt(prompt("What is your first number? "))
-    second_number = parseInt(prompt("What is the second number? "))
+// while (true) {
+//     user_input = prompt("Choose done if you would like to quit? ")
 
-    if (operation == ("*")) {
-        prompt(first_number * second_number)
-    } else if (operation == ("+")) {
-        prompt(first_number + second_number )
-    } else if (operation == ("-")) {
-        prompt (first_number - second_number)
-    } else if (operation == ("/")) {
-        prompt (first_number / second_number)
-    }
-}
+//     if (user_input == ("done")) {
+//         prompt("Good-bye")
+//         break
+//     }
+
+//     operation = prompt("What is the operation you would like to perform? ")
+//     first_number = parseInt(prompt("What is your first number? "))
+//     second_number = parseInt(prompt("What is the second number? "))
+
+//     if (operation == ("*")) {
+//         prompt(first_number * second_number)
+//     } else if (operation == ("+")) {
+//         prompt(first_number + second_number )
+//     } else if (operation == ("-")) {
+//         prompt (first_number - second_number)
+//     } else if (operation == ("/")) {
+//         prompt (first_number / second_number)
+//     }
+// }
     
 
 
