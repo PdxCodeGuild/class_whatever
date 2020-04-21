9dfc97c805b22827711ce500ed12d5a9250d991f
JoHannah Joy <jojojoy2012@gmail.com>
2020-04-20 17:34:09 -0700

    complete Lab2 VersionB and begin Lab4 ToDo List

--- code/johannah/JavaScript/simplecalc-lab-1/B-Version/scripts.js
+++ code/johannah/JavaScript/simplecalc-lab-1/B-Version/scripts.js
@@ -1,6 +1,6 @@
 // let user_input = document.getElementById("user_input");
 let num1 = document.getElementById("num1");
-// let op_input = document.getElementById("op_input");
+let op_input = document.getElementById("op_input");
 let num2 = document.getElementById("num2");
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
@@ -12,34 +12,34 @@ let output = document.getElementById("output");
 const operation = ["+", "-", "*", "/"]
 
 btn.addEventListener("click", function() {
-  while (true) {
-    let op_input = document.getElementById("op_input");
-    if (op_input === "done") {
-      break;
-    } else {
-      num1 = parseFloat(num1);
-      num2 = parseFloat(num2);
-      if (op_input === "+") {
-        const add = num1 + num2
-        // alert(`${num1} ${op_input} ${num2} = ${add}`);
-        // let output = document.getElementById("output")
-        output.innerText = `${num1} ${op_input} ${num2} = ${add}`;
+  let op_input = document.getElementById("op_input");
+  if (op_input === "+") {
+    const add = num1 + num2
+    // alert(`${num1} ${op_input} ${num2} = ${add}`);
+    // let output = document.getElementById("output")
+    // output.innerText = `${num1} ${op_input} ${num2} = ${add}`;
+    output.innerText = add;
+    // const added = `${num1} ${op_input} ${num2} = ${add}`;
+    // document.getElementById(output).appendChild(added);
+    // p.appendChild(added);
+    // p.show(added);
 
-      } else if (op_input === "-") {
-        const subtract = num1 - num2
-        output.innerText = `${num1} ${op_input} ${num2} = ${subtract}`;
-        
-      } else if (op_input === "*") {
-        const multiply = num1 * num2
-        output.innerText = `${num1} ${op_input} ${num2} = ${multiply}`;
-        
-      } else if (op_input === "/") {
-        const divide = num1 / num2
-        output.innerText = `${num1} ${op_input} ${num2} = ${divide}`;
-        
-      } else {
-        output.innerText = "Invalid input";
-      }
-    }
+  } else if (op_input === "-") {
+    const subtract = num1 - num2
+    // output.innerText = `${num1} ${op_input} ${num2} = ${subtract}`;
+    output.innerText = subtract;
+    
+  } else if (op_input === "*") {
+    const multiply = num1 * num2
+    // output.innerText = `${num1} ${op_input} ${num2} = ${multiply}`;
+    output.innerText = multiply;
+    
+  } else if (op_input === "/") {
+    const divide = num1 / num2
+    // output.innerText = `${num1} ${op_input} ${num2} = ${divide}`;
+    output.innerText = divide;
+    
+  } else {
+    output.innerText = "Invalid input";
   }
 });
