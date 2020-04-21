6844d8db5efa1c23b2a322643c60e7458ac27a57
JoHannah Joy <jojojoy2012@gmail.com>
2020-04-21 12:22:20 -0700

    complete JD lab3 versionB. Did not need to change result_output until end for innerText

--- code/johannah/JavaScript/rot13-lab-3/B-Version/scripts.js
+++ code/johannah/JavaScript/rot13-lab-3/B-Version/scripts.js
@@ -2,37 +2,37 @@ let user_input = document.getElementById('user_input');
 let btn = document.getElementById("btn");
 let output = document.getElementById('output');
 
+// let input_val = user_input.value;
+
 let result_output = ''
 // let user_input = prompt("Enter a word/phrase/sentence (letters only) to find out each character's ROT13 correspondence: ")
-const input_lowered = user_input.toLowerCase()
-// alert(input_lowered)
+// const input_lowered = input_val.toLowerCase();
 
 const chars = 'abcdefghijklmnopqrstuvwxyz'
 
 btn.addEventListener("click", function() {
+  let input_val = user_input.value;
+  const input_lowered = input_val.toLowerCase();
+  
   for (let i=0; i<input_lowered.length; i++) {
-    // console.log(i)
     let index_of_letter = parseInt(chars.search(input_lowered[i]))
-    // console.log(index_of_letter)
     let index_for_chars = index_of_letter + 13
-    // console.log(index_for_chars)
+
     if ((index_for_chars) > 25) {
       let index_of_chars_thirteen = index_for_chars - 26
-      // console.log(index_of_chars_thirteen)
       result_output += (chars[index_of_chars_thirteen])
+      // output.innerText = result_output + (chars[index_of_chars_thirteen])
       // let result_output = result_output + (chars[index_of_chars_thirteen])
-      // result_output.concat(chars[index_of_chars_thirteen])
-      // console.log(chars[index_of_chars_thirteen])
-      // console.log(result_output)
     } else {
       result_output += (chars[index_for_chars])
+      // output.innerText = result_output + (chars[index_for_chars])
       // let result_output = result_output + (chars[index_for_chars])
-      // result_output.concat(chars[index_for_chars])
-      // console.log(chars[index_for_chars])
-      // console.log(result_output)
     }
   }
+  output.innerText = result_output
+  // console.log(result_output)
 })
 
-
-console.log(result_output)
+// output.innerText = result_output
+// console.log(result_output)
+// console.log(output)
