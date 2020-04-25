563df8f74af410442ac0c9e6ea50b89a11bb3a52
JoHannah Joy <jojojoy2012@gmail.com>
2020-04-24 20:09:45 -0700

    work on JS To Do List lab4

--- code/johannah/JavaScript/todo-list/main.js
+++ code/johannah/JavaScript/todo-list/main.js
@@ -4,32 +4,44 @@ let added = document.getElementById("added");
 let completed = document.getElementById("completed");
 // let completed_btn = document.createElement("button");
 
-let input_val = user_input.val;
+// let input_val = user_input.value;
 
 
 btn.addEventListener("click", function() {
-  // append to 'added'
-  // let input_val = user_input.val;
-  let create_add = document.createElement("li");
-  let user_add = document.createTextNode(input_val);
-  create_add.appendChild(user_add);
-  let completed_btn = document.createElement("button");
+  // this all needs to be appended to 'added'
+  let input_val = user_input.value;
+  // console.log(input_val)
+  let create_add = document.createElement("p");
+  let add_input = document.createTextNode(input_val);
+  create_add.appendChild(add_input);
+  let completed_btn = document.createElement("input");
+  completed_btn.type = "button";
   completed_btn.value = "completed";
   create_add.appendChild(completed_btn);
-  let delete_btn = document.createElement("button");
+  added.appendChild(create_add);
+  let delete_btn = document.createElement("input");
+  delete_btn.type = "button";
   delete_btn.value = "remove";
   create_add.appendChild(delete_btn);
+  added.appendChild(create_add);
+
+  function clear_input() {
+    if(document.getElementById) {
+    document.user_input.reset();
+    }
+    }
+  
 })
 
 function completed_task(input_val) {
   document.getElementById("completed").appendChild(input_val.parentNode);
-  input_val.remove();
+  input_val.remove();   // removes the "completed" button
   input_val.strike();
   // let complete = input_val.strike();
   // document.getElementById("ul").innerHTML = complete;
 }
 
-function delete_me() {}
+// function delete_me() {}
 
 
 // element.appendChild(child)
@@ -40,5 +52,5 @@ function delete_me() {}
 
 
 // btn.addEventListener("click", addListAfterClick);
-delete_btn.addEventListener("click", delete_me);
-completed_btn.addEventListener("click", completed_task);
+// delete_btn.addEventListener("click", delete_me);
+// completed_btn.addEventListener("click", completed_task);
