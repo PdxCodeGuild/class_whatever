e059e7435dea863e548da4d8b7c247b18dcf0f51
Allyson <Cl0udF1sh@hotmail.com>
2020-04-28 14:45:42 -0700

    -_-

--- code/allyson/JavaScript/Lab13 - Quote Thing/quotes.js
+++ code/allyson/JavaScript/Lab13 - Quote Thing/quotes.js
@@ -1,17 +1,6 @@
-  let vm = new Vue({
-      el: '#app',
-      data: {
-          quoteResponse: {}
-      },
-      methods: {
-          load-new-quote: function () {
-              axios({
-                  method: "get",
-                  url: "http://quotes.stormconsultancy.co.uk/random.json",
-              }).then(response => {
-                  this.quoteResponse = response.data;
-              })
-          }
-      },
-  });
-  //http://quotes.stormconsultancy.co.uk/random.json//
+let vm=new Vue({
+    el:'#app',
+    data:{
+        quoteResponse:{}
+    }
+})
