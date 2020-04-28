b28f1ef21d96471e522e68dccfebcc22fdcfe695
TinaShreves <tina.l.shreves@gmail.com>
2020-04-28 11:56:46 -0700

    java

--- code/tina/javascript/lab13/main.js
+++ code/tina/javascript/lab13/main.js
@@ -1,16 +1,59 @@
+Vue.component('quotes',{
+  data () {
+    return {
+      info: [],
+      author: '',
+      last_page: false
+    }
+  },
+  template:
+  
+  `<div class="class">
+    <input placeholder='author' type='Text' v-model='author'>
+    <button @click="getData">Quote</button>
+    <ul>
+      <li v-for="object in info">
+        {{object.body}}
+      </li>
+    </ul>
+
+  </div>
+  `
+  ,
+  methods:{
+    getData: function() {
+      axios({
+        method:"get",
+        url:"https://favqs.com/api/quotes",
+        headers: {
+          Authorization:
+          'Token token="486c11b9bd4c82f3fce1627f2897f3b5"'
+        },
+        params: {
+          fliter:this.author,
+          type: 'author'
+        }
+      }).then(response =>{
+        this.info = response.data.quotes;
+        
+      })
+      
+    }
+  }
+  
+  
+})
 
   new Vue({
     el: '#app',
     data () {
       return {
-        info: {}
+        info: [],
       }
     },
-    mounted () {
-      axios
-        .get('https://favqs.com/api/quotes')
-        .then(response => (this.info = response))
-        .header()
-        
-    }
+    
   })
+
+
+
+ 
