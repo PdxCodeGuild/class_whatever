93e07fb558ba3abdff3090bc59126404eb61e04d
Merritt Lawrenson <merrittlawrenson@gmail.com>
2020-04-30 15:47:43 -0700

    drf blog

---
+++ code/merritt/feb-20-drf-blog/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics, viewsets
+
+from posts.models import Post
+from .serializers import PostSerializer
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
