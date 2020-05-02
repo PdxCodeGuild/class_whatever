f60e523e8a3c9f59967027c774be3424b93abc57
JoHannah Joy <jojojoy2012@gmail.com>
2020-05-02 12:43:26 -0700

    re-working Vue Quotes

--- code/johannah/JavaScript/Lab13-Quotes-Vue/main.js
+++ code/johannah/JavaScript/Lab13-Quotes-Vue/main.js
@@ -5,27 +5,43 @@
 // -When the page first loads, show the user a set of completely random quotes.
 // -You must have at least one Vue component in your app.
 
-let vm = new Vue({
-  el: '#app',
-  data: {
-    message: 'Enter a search term to find a quote:',
-    user_input: '',
-    author: '',
-    reveal: [],
+Vue.component('quotes', {
+  data: function () {
+    return {
+      // user_input: '',
+      author: '',
+      reveal: [],  // array of the: quotes
+    }
   },
+  template:
+    `<h3>Enter a keyword or author to find a quote:</h3>
+    <input v-model="user_input" id="user_input" type="text">
+    <button v-on:click="quote_btn">reveal quotes!</button>`
+,
+// let vm = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Enter a keyword or author to find a quote:',
+//     user_input: '',
+//     author: '',
+//     reveal: [],  // array of the: quotes
+//   },
   methods: {
     quote_btn: function() {
-      // axios({
-      //   method: 'get',
-      //   url: 'https://favqs.com/api/qotd',
-      // })
-      mounted() {
-        axios
-        .get('https://favqs.com/api/qotd')
-        .then(response => {
-          this.reveal = response.data.quote
-        }),
-      },
+      axios({
+        method: 'get',
+        url: 'https://favqs.com/api/qotd',
+      })
+      // mounted() {
+      //   axios
+      //   .get('https://favqs.com/api/qotd')
+      //   .then(response => {
+      //     this.reveal = response.data.quote
+      //   }),
+      // },
+      // axios
+      //   .get('https://favqs.com/api/qotd')
+      //   .then(response => (this.reveal = response.data))
     },
   },
 })
