424e6ccba8022c6a53c87b1263d5aebb572d13fe
JoHannah Joy <jojojoy2012@gmail.com>
2020-06-12 13:34:44 -0700

    url shortener almost complete. just keep getting a variation of this error Reverse for 'resulting' with arguments '('6', 'J', 'x', 'P', 'l', 'C')' not found. 1 pattern(s) tried: ['resulting/(?P<id_short>[^/]+)/$']

--- code/johannah/url_shortener_lab2/url_app/urls.py
+++ code/johannah/url_shortener_lab2/url_app/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'url_app'
 
 urlpatterns = [
-  # path('', views.index, name='index'),
-  # path('submit/<int:pk>', views.submit, name='submit'),
+  path('', views.index, name='index'),
+  path('submission/', views.submission, name='submission'),
+  path('resulting/<str:id_short>/', views.resulting, name='resulting'),
+  path('<str:short_url>/', views.redirection, name='redirection')
 ]
--- code/johannah/url_shortener_lab2/url_app/views.py
+++ code/johannah/url_shortener_lab2/url_app/views.py
@@ -1,22 +1,43 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+import string
+import random
+from django.utils import timezone
 
 from .models import Storage
 
 # Create your views here.
 
-# def index(request):
+def index(request):
+    short_url = ''
+    context = {'short_url': short_url}
+    return render(request, 'index.html', context)
 
 
-# def submit(request, pk):
+def submission(request):
+    short_url = ''
+    short_length = 6
+    # uppercase, lowecase, and digits for chars
+    chars = string.ascii_letters + string.digits
+    for x in range(short_length):
+        short_url += ''.join(random.choice(chars))
+    # long_url_input = request.POST.get('long_url')
+    # Storage.objects.create(long_url=long_url_input, short_url=short_url, created_time=timezone.now())
+    
+    # # use timezone here
+    Storage.objects.create(long_url=request.POST.get('long_url'),short_url=short_url, created_time=timezone.now())
+    return HttpResponseRedirect(reverse('url_app:resulting',args=(short_url)))
 
 
-# def redirection()
+def redirection(request, short_url):
+    short_url = Storage.objects.get(short_url)
+    # use clicks here in redirection
+    short_url.clicks += 1
+    short_url.save()
+    return HttpResponseRedirect(short_url
+    .long_url)
 
 
-
-
-# 2 views
-# -One to submit a url
-# -And one to redirect the user
-# Use django's HTTPResponseRedirect to handle redirecting users
+def resulting(request, id_short):
+    return render(request, 'index.html', {'short_url_link':Storage.objects.get(short_url=id_short)})
