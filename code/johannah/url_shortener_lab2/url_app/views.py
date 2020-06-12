2c264cfff9f77df66a4cd4dfb1cda1a7f012b195
JoHannah Joy <jojojoy2012@gmail.com>
2020-06-12 16:15:56 -0700

    fix redirection to go to page

--- code/johannah/url_shortener_lab2/url_app/views.py
+++ code/johannah/url_shortener_lab2/url_app/views.py
@@ -18,25 +18,24 @@ def index(request):
 def submission(request):
     short_url = ''
     short_length = 6
-    # uppercase, lowecase, and digits for chars
+    # uppercase, lowercase, and digits for chars
     chars = string.ascii_letters + string.digits
     for x in range(short_length):
         short_url += ''.join(random.choice(chars))
     # long_url_input = request.POST.get('long_url')
     # Storage.objects.create(long_url=long_url_input, short_url=short_url, created_time=timezone.now())
-    
+
     # # use timezone here
     Storage.objects.create(long_url=request.POST.get('long_url'),short_url=short_url, created_time=timezone.now())
-    return HttpResponseRedirect(reverse('url_app:resulting',args=(short_url)))
+    return HttpResponseRedirect(reverse('url_app:resulting',args=(short_url,)))
 
 
 def redirection(request, short_url):
-    short_url = Storage.objects.get(short_url)
+    short_url = Storage.objects.get(short_url=short_url)
     # use clicks here in redirection
-    short_url.clicks += 1
+    short_url.num_clicks += 1
     short_url.save()
-    return HttpResponseRedirect(short_url
-    .long_url)
+    return HttpResponseRedirect(short_url.long_url)
 
 
 def resulting(request, id_short):
